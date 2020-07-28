8480a37640ea97e2f384bcc07ac6251c01c78623
Dmitry Karpov <dem0schase@gmail.com>
2020-07-29 01:18:17 +0300

    products, cart rendering added

--- project/store/models.py
+++ project/store/models.py
@@ -1,9 +1,11 @@
+import os.path
 from django.db import models
 from django.contrib.auth.models import User
+from django.conf import settings
 
 
 class Customer(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True, related_name='customer')
     name = models.CharField(max_length=200, null=True)
     email = models.CharField(max_length=200)
 
@@ -24,9 +26,18 @@ class Product(models.Model):
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=True)
     info = models.TextField(max_length=5000)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return f'ID: {self.id}. {self.name}'
+
+    @property
+    def image_path(self):
+        if self.image:
+            return self.image.url
+        else:
+            generic_product_image_filename = 'placeholder.png'
+            return os.path.normpath(os.path.join(settings.MEDIA_URL, generic_product_image_filename))
 
 
 class Order(models.Model):
@@ -36,19 +47,38 @@ class Order(models.Model):
     transaction_id = models.CharField(max_length=200, null=True)
 
     def __str__(self):
-        return f'ID: {self.id}, customer: {self.customer.name}{", complete" if self.complete else ""}'
+        return f'ID: {self.id}, customer: {self.customer.name}, items count: {self.quantity}, ' \
+               f'total amount: {self.amount}{", complete" if self.complete else ""}'
 
     @property
     def amount(self):
         return sum(item.amount for item in self.items.all())
 
+    @property
+    def quantity(self):
+        return sum(item.quantity for item in self.items.all())
 
-class OrderItem():
+
+class OrderItem(models.Model):
     product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True, related_name='items')
     quantity = models.IntegerField(default=0, null=True, blank=True)
     date_added = models.DateTimeField(auto_now_add=True)
-    amount = models.FloatField()
+
+    @property
+    def amount(self):
+        return self.product.price * self.quantity
 
     def __str__(self):
         return f'ID: {self.id}, order ID: {self.order.id}, product: {self.product.name}, amount: {self.amount}'
+
+
+class ShippingAddress(models.Model):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'ID: {self.id}, order ID: {self.order.id}, customer: {self.customer.name}, address: {self.address}'
--- project/store/views.py
+++ project/store/views.py
@@ -1,18 +1,33 @@
 from django.shortcuts import render
+from .models import *
+
 
 def store(request):
     context = {
         'title': 'This is store',
         'body': 'This is store page',
+        'products': Product.objects.all(),
     }
     return render(request, 'store/store.html', context)
 
+
 def cart(request):
+    if request.user.is_authenticated:
+        customer = request.user.customer
+        order, created = Order.objects.get_or_create(customer=customer, complete=False)
+        items = order.items.all()
+    else:
+        items = []  # TODO - implement logic later
+        order = None
+
     context = {
         'title': 'This is cart',
         'body': 'This is cart page',
+        'order': order,
+        'items': items,
     }
     return render(request, 'store/cart.html', context)
+
 
 def checkout(request):
     context = {
